d95e4fd17170ff58e2be88143a7653f12bc6f3bd
WuSuJie <2020554057@qq.com>
2023-12-28 16:40:34 +0800

    feat：优惠券秒杀（Redis创建全局唯一Id,使用乐观锁解决超卖问题）

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +27,56 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        //1、查询秒杀券信息
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        //2、判断秒杀是否开始
+        if (seckillVoucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            //未开始
+            return Result.fail("秒杀未开始！");
+        }
+        //3、判断秒杀是否结束
+        if (seckillVoucher.getEndTime().isBefore(LocalDateTime.now())) {
+            //结束了
+            return Result.fail("秒杀已结束！");
+        }
+        //4、判断是否有库存
+        if (seckillVoucher.getStock() < 1) {
+            //库存未空
+            return Result.fail("优惠券已经抢完了！");
+        }
+        //5、扣减库存
+        boolean flag = seckillVoucherService.update()
+                .setSql("stock = stock -1")
+                .eq("voucher_id", voucherId).gt("stock",0)//TODO ：这里使用到了乐观锁解决并发超卖的问题
+                .update();
+        if (!flag) {
+            return Result.fail("秒杀券扣减失败!");
+        }
+        //6、创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        //6.1、订单id
+        long orderID = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderID);
+        //6.2、用户id
+        Long userID = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userID);
+        //6.3、优惠券id
+        voucherOrder.setVoucherId(voucherId);
+        //7、向数据库中添加数据
+        boolean isCreate = save(voucherOrder);
+        if (!isCreate) {
+            return Result.fail("创建秒杀券订单失败！");
+        }
+
+        //7、返回订单id
+        return Result.ok(voucherOrder.getId());
+    }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,18 +1,51 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Autowired
     private ShopServiceImpl shopService;
+    @Autowired
+    private RedisIdWorker redisIdWorker;
 
+    //线程池
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
+
+    /**
+     * 模拟单元测试，解决热点Key问题，先将数据导入到redis中
+     * @throws InterruptedException
+     */
     @Test
     public void testSaveShop2Redis() throws InterruptedException {
         shopService.saveShop2Redis(1L,10L);
